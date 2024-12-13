91dc021db932204b9b607b16dbc449ec1615d354
Micjeal <142317765+Micjeal@users.noreply.github.com>
2024-12-13 11:42:59 +0300

    Add project files.

---
+++ CRUD APPLICATION.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CRUD_APPLICATION.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CRUD APPLICATION/Controllers/HOMEController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_APPLICATION.Models;
+
+namespace CRUD_APPLICATION.Controllers
+{
+    public class HOMEController : Controller
+    {
+
+        crudDBEntities1 DatabaseEFObject = new crudDBEntities1();
+        // GET: HOME
+        public ActionResult Index()
+        {
+            var IndexData = DatabaseEFObject.Employeetbls.ToList();
+            return View(IndexData);
+        }
+    }
+}
